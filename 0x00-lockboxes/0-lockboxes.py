d10ed355c03cc1b5588454a9dbb240a4312408bb
VMP1312 <60369844+VMP1312@users.noreply.github.com>
2020-12-08 04:31:41 -0500

    This should work

---
+++ 0x00-lockboxes/0-lockboxes.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+Module.
+"""
+
+def canUnlockAll(boxes):
+    """Determines if all the boxes can be opened"""
+
+    key = 0
+    holder = [0]
+    if len(boxes) == 0:
+        return True
+
+    for key in holder:
+        for key in boxes[key]:
+            
+            if key in holder:
+                continue
+
+            else:
+                if key <= len(boxes) - 1:
+                    holder.append(key)
+
+                else:
+                    continue
+
+    if len(boxes) != len(holder):
+        return False
+    else:
+        return True
