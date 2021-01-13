ae1f86397d15b190dcef27deb2e3d320f4f4ac81
VMP1312 <victor.paz1312@hotmail.com>
2021-01-13 11:46:32 -0500

    UPLOAD

---
+++ 0x04-sandpiles/0-sandpiles.c
@@ -0,0 +1,87 @@
+#include "sandpiles.h"
+
+/**
+* print_grid - printer for the first grid
+* @grid: grid 1
+*/
+
+static void printer(int grid[3][3])
+{
+    int i;
+    int j;
+
+    for (i = 0; i < 3; i++)
+    {
+        for (j = 0; j < 3; j++)
+        {
+            if (j)
+            printf(" ");
+            printf("%d", grid[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+/**
+ * sandpiles_sum - sum sandpiles
+ * @grid1: grid 1
+ * @grid2: grid 2
+ */
+void sandpiles_sum(int grid1[3][3], int grid2[3][3])
+{
+    int i = 0;
+    int j = 0;
+    int stl = 0;
+    int cnt = 0;
+    int check = 0;
+
+    while (cnt == 0)
+    {
+        stl = 0;
+        if (check != 0)
+        {
+            for (i = 0; i < 3; i++)
+            {
+                for (j = 0; j < 3; j++)
+                {
+                    grid2[i][j] = grid1[i][j];
+                    if (grid1[i][j] <= 3)
+                        stl++;
+                }
+            }
+
+            if (stl == 9)
+            break;
+            printf("=\n");
+            printer(grid1);
+        }
+
+        stl = 0;
+        for (i = 0; i < 3; i++)
+        {
+            for (j = 0; j < 3; j++)
+            {
+                if (check != 0)
+                {
+                    if (grid2[i][j] > 3)
+                    {
+                        grid1[i][j] = grid1[i][j] - 4;
+                        if (j > 0)
+                            grid1[i][j - 1] = (grid1[i][j - 1]) + 1;
+                        if (i > 0)
+                            grid1[i - 1][j] = (grid1[i - 1][j]) + 1;
+                        if (i < 2)
+                            grid1[i + 1][j] = (grid1[i + 1][j]) + 1;
+                        if (j < 2)
+                            grid1[i][j + 1] = (grid1[i][j + 1]) + 1;
+                    }
+                    else
+                        stl++;
+                }
+            else
+                grid1[i][j] = grid1[i][j] + grid2[i][j];
+            }
+        }
+    check = 1;
+    }
+}
---
+++ 0x04-sandpiles/sandpiles.h
@@ -0,0 +1,12 @@
+#ifndef _SANDPILES_H
+#define _SANDPILES_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
+
+static void printer(int grid[3][3]);
+
+#endif
