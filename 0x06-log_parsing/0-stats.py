13a756a76cbd74e1e5f25a6c35b1e04a33df1a67
VMP1312 <60369844+VMP1312@users.noreply.github.com>
2021-01-27 00:33:18 -0500

    FIX

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -1,36 +1,42 @@
 #!/usr/bin/python3
-""" script that reads stdin line by line and computes metrics"""
+"""Reads stdin line by line and computes metrics"""
 
 import sys
 
 if __name__ == "__main__":
 
-    increase = 1
-    status_code = [0, 0, 0, 0, 0, 0, 0, 0]
+    cnt = 1
+    sCodes = [0, 0, 0, 0, 0, 0, 0, 0]
     size = 0
-    status_L = ["200", "301", "400", "401", "403", "404", "405", "500"]
+    sList = ["200", "301", "400", "401", "403", "404", "405", "500"]
 
-    def signal_print(status_code, status_L):
-        """ Method to handle ctrl-c"""
+    def printer(sCodes, status_L):
+        """Printer method"""
+
         print("File size: {}".format(size))
-        for i in range(len(status_code)):
-            if status_code[i] > 0:
-                print("{}: {}".format(status_L[i], status_code[i]))
+        for mv in range(len(sCodes)):
+            if sCodes[mv] > 0:
+                print("{}: {}".format(sList[mv], sCodes[mv]))
 
     try:
         for line in sys.stdin:
-            each_argvs = line.split(" ")
-            if len(each_argvs) > 6:
-                status = each_argvs[-2]
-                if status in status_L:
-                    i = status_L.index(status)
-                    status_code[i] = status_code[i] + 1
-                size = size + int(each_argvs[-1][:])
-                if increase % 10 == 0:
-                    signal_print(status_code, status_L)
-            increase = increase + 1
+            amount = line.split(" ")
+
+            if len(amount) > 6:
+                status = amount[-2]
+
+                if status in sList:
+                    mv = sList.index(status)
+                    sCodes[mv] = sCodes[mv] + 1
+
+                size = size + int(amount[-1][:])
+                if cnt % 10 == 0:
+                    printer(sCodes, sList)
+
+            cnt = cnt + 1
+
     except KeyboardInterrupt:
-        signal_print(status_code, status_L)
+        printer(sCodes, sList)
         raise
-    signal_print(status_code, status_L)
-    
+
+    printer(sCodes, sList)
