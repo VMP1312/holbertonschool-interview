ff2c666902945eb5c2148e266a1ed29217d20099
VMP1312 <60369844+VMP1312@users.noreply.github.com>
2021-01-27 00:27:58 -0500

    Try

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -5,38 +5,32 @@ import sys
 
 if __name__ == "__main__":
 
-    cnt = 1
-    sCodes = [0, 0, 0, 0, 0, 0, 0, 0]
+    increase = 1
+    status_code = [0, 0, 0, 0, 0, 0, 0, 0]
     size = 0
-    sList = ["200", "301", "400", "401", "403", "404", "405", "500"]
+    status_L = ["200", "301", "400", "401", "403", "404", "405", "500"]
 
-    def printer(sCodes, status_L):
-        """Printer method"""
-
+    def signal_print(status_code, status_L):
+        """ Method to handle ctrl-c"""
         print("File size: {}".format(size))
-        for mv in range(len(sCodes)):
-            if sCodes[mv] > 0:
-                print("{}: {}".format(sList[mv], sCodes[mv]))
+        for i in range(len(status_code)):
+            if status_code[i] > 0:
+                print("{}: {}".format(status_L[i], status_code[i]))
 
     try:
         for line in sys.stdin:
-            amount = line.split(" ")
-
-            if len(amount) > 6:
-                status = amount[-2]
-
-                if status in sList:
-                    mv = sList.index(status)
-                    sCodes[mv] = sCodes[mv] + 1
-
-                size = size + int(amount[-1][:])
-                if cnt % 10 == 0:
-                    printer(sCodes, sList)
-
-            cnt = cnt + 1
-
+            each_argvs = line.split(" ")
+            if len(each_argvs) > 6:
+                status = each_argvs[-2]
+                if status in status_L:
+                    i = status_L.index(status)
+                    status_code[i] = status_code[i] + 1
+                size = size + int(each_argvs[-1][:])
+                if increase % 10 == 0:
+                    signal_print(status_code, status_L)
+            increase = increase + 1
     except KeyboardInterrupt:
-        printer(sCodes, sList)
+        signal_print(status_code, status_L)
         raise
-
-    printer(sCodes, sList)
+    signal_print(status_code, status_L)
+    
