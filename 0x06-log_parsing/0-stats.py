f2803bfd23ac1b400b938c595403319d89518472
VMP1312 <60369844+VMP1312@users.noreply.github.com>
2021-01-27 22:30:18 -0500

    New way

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -3,40 +3,44 @@
 
 import sys
 
+data = {"Size": 0, "Codes": {"200": 0, "301": 0, "400": 0, "401": 0,
+                             "403": 0, "404": 0, "405": 0, "500": 0}}
+
+
+def printer():
+    """Printer"""
+
+    print("File size: {}".format(data['Size']))
+
+    for sCode, mv in sorted(data['Codes'].items()):
+        if mv != 0:
+            print("{}: {}".format(sCode, mv))
+
+
 if __name__ == "__main__":
-
-    cnt = 1
-    sCodes = [0, 0, 0, 0, 0, 0, 0, 0]
-    size = 0
-    sList = ["200", "301", "400", "401", "403", "404", "405", "500"]
-
-    def printer(sCodes, status_L):
-        """Printer method"""
-
-        print("File size: {}".format(size))
-        for mv in range(len(sCodes)):
-            if sCodes[mv] > 0:
-                print("{}: {}".format(sList[mv], sCodes[mv]))
-
     try:
+        cnt = 1
         for line in sys.stdin:
-            amount = line.split(" ")
 
-            if len(amount) > 6:
-                status = amount[-2]
+            nLine = line.split(" ")
 
-                if status in sList:
-                    mv = sList.index(status)
-                    sCodes[mv] = sCodes[mv] + 1
+            try:
+                size = int(nLine[-1])
+                code = nLine[-2]
 
-                size = size + int(amount[-1][:])
-                if cnt % 10 == 0:
-                    printer(sCodes, sList)
+                if code in data['Codes']:
+                    data['Codes'][code] += 1
 
-            cnt = cnt + 1
+                data['Size'] += size
+            except:
+                pass
+
+            if cnt % 10 == 0:
+                printer()
+
+            cnt += 1
 
     except KeyboardInterrupt:
-        printer(sCodes, sList)
+        printer()
         raise
-
-    printer(sCodes, sList)
+    printer()
