bf3c37718afc97def2aaf60fdcdaafbdf84b62d6
VMP1312 <60369844+VMP1312@users.noreply.github.com>
2021-02-03 01:19:09 -0500

    UPLOAD

---
+++ 0x07-linked_list_cycle/0-check_cycle.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+/**
+ * check_cycle - Checks if a single link list has a cycle in it.
+ * @list: List.
+ * Return: 0 if there is no cycle, 1 if there is a cycle.
+ *
+ */
+
+int check_cycle(listint_t *list)
+{
+	listint_t *slow;
+	listint_t *fast;
+
+	if (list == NULL)
+		return (0);
+	slow = list;
+	fast = list;
+	while (slow->next != NULL && fast->next != NULL && fast->next->next != NULL)
+	{
+		slow = slow->next;
+		fast = n2->next->next;
+		if (slow == fast)
+			return (1);
+	}
+	return (0);
+}
