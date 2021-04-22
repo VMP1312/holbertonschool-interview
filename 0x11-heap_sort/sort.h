8bccd684fb273137e0de959feca50e8a7b744311
VMP1312 <victor.paz1312@hotmail.com>
2021-04-22 14:21:05 -0500

    UPLOAD

---
+++ 0x11-heap_sort/sort.h
@@ -0,0 +1,9 @@
+#ifndef heap_sort_h
+#define heap_sort_h
+
+void heap_sort(int *array, size_t size);
+void maximum_heap(int *array, int idx, int len, int size);
+void swapper(int *a, int *b);
+void print_array(const int *array, size_t size);
+
+#endif
