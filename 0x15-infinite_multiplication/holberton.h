61eed6d74e7014da37bb48fbd5beb8e92881012e
VMP1312 <victor.paz1312@hotmail.com>
2021-05-20 21:58:18 -0500

    UPLOAD

---
+++ 0x15-infinite_multiplication/holberton.h
@@ -0,0 +1,11 @@
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
+
+#include <stdlib.h>
+
+int _putchar(char c);
+int *infi_multi(char *n1, int len1, char *n2, int len2);
+void printter(char *);
+int num_cnt_len(char *num);
+
+#endif
